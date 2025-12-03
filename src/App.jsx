60562840cb5aa70f19d3e3b959327176de36e20f
rusmiarahaman77461@gmail.com <rusmiarahaman77461@gmail.com>
2025-12-03 18:34:34 +0600

    feat: import ClockStopwatchApp

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import ClockStopwatchApp from './assets/components/clock';
+
+function App(){
+
+  return(
+
+    <ClockStopwatchApp/>
+
+  )
+
+}
+
+export default App;
